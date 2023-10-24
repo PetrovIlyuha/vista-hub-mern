5552b4fbbe999b86de7029eafb6cd3fe326aae7f
Petrov Ilya <dnamix1@gmail.com>
2023-10-24 14:13:09 +0300

    pages * routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { SignIn } from "./pages/SignIn";
+import { About } from "./pages/About";
+import { Profile } from "./pages/Profile";
+import { SignUp } from "./pages/SignUp";
+
 function App() {
   return (
-    <div className="w-full h-screen flex items-center justify-center bg-red-400 text-white">
-      <h1 className="text-4xl font-bold animate-pulse">Vista-Hub</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
