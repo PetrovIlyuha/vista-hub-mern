399bbea4b9739704bfe70d7dce344badfa8215d2
Petrov Ilya <dnamix1@gmail.com>
2023-10-24 12:11:18 +0300

    project created, tailwind integrated

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-red-400 text-white">
+      <h1 className="text-4xl font-bold animate-ping">Vista-Hub</h1>
+    </div>
+  );
+}
+
+export default App;
