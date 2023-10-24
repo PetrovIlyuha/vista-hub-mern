38765cdb10be42673a157009f4d7143ed30d7d34
Petrov Ilya <dnamix1@gmail.com>
2023-10-24 12:37:53 +0300

    testing git

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="w-full h-screen flex items-center justify-center bg-red-400 text-white">
-      <h1 className="text-4xl font-bold animate-ping">Vista-Hub</h1>
+      <h1 className="text-4xl font-bold animate-pulse">Vista-Hub</h1>
     </div>
   );
 }
